3b3ff2da8594fffedb571f4e07593d5e6193dd3f
LMS.im <ihappyet@gmail.com>
2021-09-26 10:44:09 +0800

    Delete tpl directory

--- tpl/tpl-random.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Template Name: random
-*/
-?>
-<?php $rand_post=get_posts('numberposts=1&orderby=rand'); foreach($rand_post as $post) : ?>
-<script> location="<?php the_permalink(); ?>";</script>
-<?php endforeach; ?>
